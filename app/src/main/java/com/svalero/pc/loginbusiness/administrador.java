dc40ea0d668964af0fbcb0ddee25ea424b6552ae
nberrueco <nberto3@gmail.com>
2016-06-13 20:35:58 +0200

    Funciona! Prueba!

--- app/src/main/java/com/svalero/pc/loginbusiness/administrador.java
+++ app/src/main/java/com/svalero/pc/loginbusiness/administrador.java
@@ -56,7 +56,7 @@ public class administrador extends AppCompatActivity {
                 startActivity(navegar);
             }
         });
-        //
+        //work
         btnMensajeria = (Button) findViewById(R.id.btnMensajeria);
         btnMensajeria.setOnClickListener(new View.OnClickListener() {
             @Override
